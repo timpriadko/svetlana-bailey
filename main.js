2fefa5551297517e92bc85b3343f1f319a77f3c4
Artem Priadko <artem.priadko@syntech.software>
2019-05-31 11:59:39 +0300

    responsive fixes

--- main.js
+++ main.js
@@ -2,17 +2,14 @@ window.onload = function() {
     var pageHeight = document.documentElement.clientWidth * 1.49 + 'px';
     document.querySelector('.background-wrap') ? document.querySelector('.background-wrap').style.height = pageHeight : '';
 
-    if (window.screen.width >= 360) {
+    if (window.screen.width >= 1920) {
         // svetlana page
-        document.querySelector('.background-wrap') ? document.querySelector('.background-wrap').style.height = document.documentElement.clientWidth * 1.75 + 'px' : '';
+        document.querySelector('.background-wrap') ? document.querySelector('.background-wrap').style.height = document.documentElement.clientWidth * 1.5 + 'px' : '';
         // cv page
-        document.querySelector('.contacts-page-wrap') ? document.querySelector('.contacts-page-wrap').style.height = document.documentElement.clientWidth * 4.1 + 'px' : '';
+        document.querySelector('.contacts-page-wrap') ? document.querySelector('.contacts-page-wrap').style.height = document.documentElement.clientWidth * 1.1 + 'px' : '';
     }
-    if (window.screen.width >= 768) {
-        // svetlana page
-        document.querySelector('.background-wrap') ? document.querySelector('.background-wrap').style.height = document.documentElement.clientWidth * 1.6 + 'px' : '';
-        // cv page
-        document.querySelector('.contacts-page-wrap') ? document.querySelector('.contacts-page-wrap').style.height = document.documentElement.clientWidth * 2.7 + 'px' : '';
+    if (window.screen.width >= 1366) {
+        document.querySelector('.contacts-page-wrap') ? document.querySelector('.contacts-page-wrap').style.height = document.documentElement.clientWidth * 1.3 + 'px' : '';
     }
     if (window.screen.width >= 1280) {
         // svetlana page
@@ -20,14 +17,17 @@ window.onload = function() {
         // cv page
         document.querySelector('.contacts-page-wrap') ? document.querySelector('.contacts-page-wrap').style.height = document.documentElement.clientWidth * 1.7 + 'px' : '';
     }
-    if (window.screen.width >= 1366) {
-        document.querySelector('.contacts-page-wrap') ? document.querySelector('.contacts-page-wrap').style.height = document.documentElement.clientWidth * 1.3 + 'px' : '';
-    }
-    if (window.screen.width >= 1920) {
+    if (window.screen.width >= 768) {
         // svetlana page
-        document.querySelector('.background-wrap') ? document.querySelector('.background-wrap').style.height = document.documentElement.clientWidth * 1.5 + 'px' : '';
+        document.querySelector('.background-wrap') ? document.querySelector('.background-wrap').style.height = document.documentElement.clientWidth * 1.6 + 'px' : '';
         // cv page
-        document.querySelector('.contacts-page-wrap') ? document.querySelector('.contacts-page-wrap').style.height = document.documentElement.clientWidth * 1.1 + 'px' : '';
+        document.querySelector('.contacts-page-wrap') ? document.querySelector('.contacts-page-wrap').style.height = document.documentElement.clientWidth * 2.7 + 'px' : '';
+    }
+    if (window.screen.width >= 360) {
+        // svetlana page
+        document.querySelector('.background-wrap') ? document.querySelector('.background-wrap').style.height = document.documentElement.clientWidth * 1.75 + 'px' : '';
+        // cv page
+        document.querySelector('.contacts-page-wrap') ? document.querySelector('.contacts-page-wrap').style.height = document.documentElement.clientWidth * 4.1 + 'px' : '';
     }
 
     window.addEventListener('mousemove', scrollHandler);
