5609a143c5551458df3e014a2270657718128fa6
Artem Priadko <artem.priadko@syntech.software>
2019-06-06 10:49:27 +0300

    m-album orientation func

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ window.onload = function() {
         document.querySelector('.contacts-page-wrap') ? document.querySelector('.contacts-page-wrap').style.height = document.documentElement.clientWidth * 1.7 + 'px' : '';
         // mobile bg height & scroll
         document.querySelector('.main').style.height = document.documentElement.clientWidth * 0.5 + 'px';
-        document.querySelector('.first-page') ? window.scrollTo(0, 560) : window.scrollTo(0, 170);
+        document.querySelector('.first-page') ? window.scrollTo(0, 430) : window.scrollTo(0, 170);
     }
     if (window.screen.width >= 720) {
         // mobile bg height & scroll
@@ -147,7 +147,9 @@ window.onload = function() {
 
 window.addEventListener("orientationchange", function() {
     // Landscape orientation
-    if (screen.orientation.angle === 90) {
-        window.location.reload(true);
+    if (screen.orientation.angle === 90 || screen.orientation.angle === -90) {
+        console.log('+');
+        document.querySelector('.first-page') ? window.scrollTo(0, 430) : window.scrollTo(0, 170);
+        // window.location.reload(true);
     }
 });
