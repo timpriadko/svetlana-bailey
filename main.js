d25147f631585a9bd714230c5764ccf627d5ad83
Artem Priadko <artem.priadko@syntech.software>
2019-06-06 11:18:24 +0300

    m-album orientation func

--- main.js
+++ main.js
@@ -27,11 +27,11 @@ window.onload = function() {
         document.querySelector('.contacts-page-wrap') ? document.querySelector('.contacts-page-wrap').style.height = document.documentElement.clientWidth * 1.7 + 'px' : '';
         // mobile bg height & scroll
         document.querySelector('.main').style.height = document.documentElement.clientWidth * 0.5 + 'px';
-        document.querySelector('.first-page') ? window.scrollTo(0, 760) : window.scrollTo(0, 170);
+        document.querySelector('.first-page') ? window.scrollTo(0, 560) : window.scrollTo(0, 170);
     }
     if (window.screen.width >= 720) {
         // mobile bg height & scroll
-        document.querySelector('.first-page') ? window.scrollTo(0, 730) : window.scrollTo(0, 170);
+        document.querySelector('.first-page') ? window.scrollTo(0, 530) : window.scrollTo(0, 170);
     }
     if (window.screen.width >= 768 && window.screen.width < 1024) {
         // // svetlana page
